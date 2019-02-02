f643c79668d5d7a095ed781d589efc1a5c7e0ca9
Rosie Zhao <rosieyzh@gmail.com>
2019-02-02 15:18:48 -0500

    Modified Fractal

--- sketch.js
+++ sketch.js
@@ -2,20 +2,27 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	stroke(0, 255, 0);
 	background(200);
-	frameRate(60);
+	frameRate(20);
 
 	x=width / 2;
 	y=height;
+	rColor=0;
+	gColor=255;
+	bColor=0;
+	colIncrease=50;
+	noLoop();
+	weight=4;
+	weightDec=0.8;
 }
 
 function draw() {
 	background(50);
-	angle = PI / 3;
+	angle = PI/4;
 
 	//start at bottom of window, in the middle
 	translate(width/2, height);
 	stroke(255);
-	branch(120);
+	branch(height/4);
 
 
 }
@@ -23,25 +30,34 @@ function draw() {
 function branch(len) {
 	//draw branch with certain length
 	//negative len moves upwards
-	strokeWeight(2);
-	line(0,0,0+random(-1,1), -len);
+	strokeWeight(weight);
+	stroke(rColor, gColor, bColor)
+	line(0,0,0, -len);
 
 	//keeps window centered, translates up by length
 	translate(0, -len);
 	//shortens length of next branch
-	len*=0.7;
+	len*=1/sqrt(2);
 
 	if (len>2) {
 		//rotate canvas by angle
 		//recursively call branch
 		push();
+		bColor+=colIncrease;
+		weight*=weightDec;
 		rotate(angle);
 		branch(len);
+		bColor-=colIncrease;
+		weight=weight/weightDec;
 		pop();
 
 		push();
+		bColor+= colIncrease;
+		weight*= weightDec;
 		rotate(-angle);
 		branch(len);
+		bColor-= colIncrease;
+		weight= weight/weightDec;
 		pop();
 	}
 }
